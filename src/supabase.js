176c363600244a710f63a492edba082a0ccb87e3
shepot88 <didomk3@gmail.com>
2026-05-28 23:19:13 +0300

    real project url

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://aumoiucfasixxayevfsn.supabase.co/rest/v1/";
+const supabaseUrl = "https://aumoiucfasixxayevfsn.supabase.co";
 
 const supabaseKey =
   "sb_publishable_h2_46fvtul7b1HfhCO7KLA_p0nDclkX";
