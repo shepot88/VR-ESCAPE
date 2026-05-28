d28b3cddd9d13c2c2925b3b3abc5ade28c0c711d
shepot88 <didomk3@gmail.com>
2026-05-28 23:11:44 +0300

    fixed key

--- src/supabase.js
+++ src/supabase.js
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://aumoiucfasixxayevfsn.supabase.co";
 
 const supabaseKey =
-sb_publishable_h2_46fvtul7b1HfhCO7KLA_p0nDclkX
+  "sb_publishable_h2_46fvtul7b1HfhCO7KLA_p0nDclkX";
 
 export const supabase = createClient(
   supabaseUrl,
